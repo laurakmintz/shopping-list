b34c72e10d114492f5f93f7829ca7905a6de5ea5
Laura Mintz <Laura@Lauras-MacBook-Pro.local>
2014-05-30 09:51:45 -0400

    shopping list final

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-$(document).ready(function() {
-	
+$(document).ready(function(){
+	$('#entry').keyup(function(event){
+		console.log(event);
+		if(event.keyCode == 13) {
+			//console.log("hello");
+			event.preventDefault();
+			var txtbox = document.getElementById('entry');
+			var txtval = txtbox.value;
+			$('#items').append('<li>'+txtval+'</li>');
+		};
+	});
+	$('#add').click(function(){
+		event.preventDefault();
+		var txtbox = document.getElementById('entry');
+		var txtval = txtbox.value;
+		$('#items').append('<li>'+txtval+'</li>');
+	});
+	$('.list').on('click', 'li', function(){
+		//console.log("strike");
+		$(this).wrap("<strike>");
+	});	
+	$('.list').on('dblclick', 'li', function(){
+		//console.log("working");
+		$(this).remove();
+	});
+});
+
+
+
+
+
